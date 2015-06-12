d4856b412034cfa9aa01ff4598c73618b8c97688
Jaime Cham <jaime.cham@cardinalblue.com>
2015-06-12 10:53:27 +0800

    installed parse module

--- HotlineParse/cloud/main.js
+++ HotlineParse/cloud/main.js
@@ -1,6 +1,25 @@
+// ------------------------
+// See https://github.com/layerhq/layer-parse-module
+//
+
+var fs = require('fs');
+var layer = require('cloud/layer-parse-module/layer-module.js');
+
+var layerProviderID = 'adfadd6e-1014-11e5-a5df-9fa92d003905';
+var layerKeyID      = 'ab4eaa84-10ad-11e5-9172-9fa92d006e08';
+var privateKey      = fs.readFileSync('cloud/layer-parse-module/keys/layer-key.js');
+layer.initialize(layerProviderID, layerKeyID, privateKey);
+
+Parse.Cloud.define("generateToken", function(request, response) {
+    var userID = request.params.userID;
+    var nonce = request.params.nonce;
+    if (!userID) throw new Error('Missing userID parameter');
+    if (!nonce) throw new Error('Missing nonce parameter');
+        response.success(layer.layerIdentityToken(userID, nonce));
+});
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
+// Parse.Cloud.define("hello", function(request, response) {
+//   response.success("Hello world!");
+// });
