6ea30385722211d9d11517e90893e1e50386f9d7
Tyler Barth <divergio@gmail.com>
2015-06-12 13:10:11 +0800

    Basic UI elements

---
+++ Code/Controllers/HOTConversationViewController.h
@@ -0,0 +1,13 @@
+//
+//  HOTConversationViewController.h
+//  Layer-Parse-iOS-Example
+//
+//  Created by Tyler Barth on 2015-06-12.
+//  Copyright (c) 2015年 Layer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HOTConversationViewController : UIViewController
+
+@end
