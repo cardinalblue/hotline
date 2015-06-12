418eb2a66d26eae5a60a3868cb9c6c42b595e8a7
Jaime Cham <jaime.cham@cardinalblue.com>
2015-06-12 14:12:14 +0800

    added HOTAdditions category

---
+++ Code/Categories/LYRClient+HOTAdditions.m
@@ -0,0 +1,13 @@
+//
+//  LYRClient+HOTAdditions.m
+//  Layer-Parse-iOS-Example
+//
+//  Created by Jaime Cham on 6/12/15.
+//  Copyright (c) 2015 Layer. All rights reserved.
+//
+
+#import "LYRClient+HOTAdditions.h"
+
+@implementation LYRClient (HOTAdditions)
+
+@end
